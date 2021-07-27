bcdc2c29fe91825e480af677fc28af0f7867f3ed
yunweneric <yunweneric@gmail.com>
2021-07-27 11:57:53 +0100

    animated onboarding

--- lib/presentation/screens/onboarding/OnBoadingPage.dart
+++ lib/presentation/screens/onboarding/OnBoadingPage.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -63,6 +64,7 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
       fontSize: 16.0,
       fontFamily: 'lato',
       height: 1.5,
+      color: Color(0xff000000),
     );
 
     const pageDecoration = const PageDecoration(
@@ -92,42 +94,145 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
 
       pages: [
         PageViewModel(
-          title: "Hi! Friend",
-          body:
-              "I am Greeniser! \nI will be teaching you several things these days and I will help you become Ecofriendly, and aiding you save energy at home ",
+          titleWidget: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Hi ",
+                    style: pageDecoration.titleTextStyle,
+                  ),
+                  DefaultTextStyle(
+                    style: pageDecoration.titleTextStyle,
+                    child: AnimatedTextKit(
+                      pause: Duration(seconds: 1),
+                      repeatForever: true,
+                      animatedTexts: [
+                        TyperAnimatedText(
+                          'Friend!',
+                          speed: Duration(
+                            milliseconds: 100,
+                          ),
+                        ),
+                        TyperAnimatedText(
+                          'Activist!',
+                          speed: Duration(
+                            milliseconds: 100,
+                          ),
+                        ),
+                        TyperAnimatedText(
+                          'Advocate!',
+                          speed: Duration(
+                            milliseconds: 100,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                "I am Greeniser! \nI will be teaching you several things these days and I will help you become Ecofriendly, and aiding you save energy at home ",
+                style: pageDecoration.bodyTextStyle,
+                textAlign: TextAlign.center,
+              )
+            ],
+          ),
+          bodyWidget: Text(
+            "",
+            style: pageDecoration.bodyTextStyle,
+            textAlign: TextAlign.center,
+          ),
           image: _buildImage2('undraw_female_avatar_w3jk.svg'),
           decoration: pageDecoration,
         ),
         PageViewModel(
-          title: "Manage waste properly",
-          body:
-              "Proper waste management in an environmental friendly manner, preventing uncesssasary wastage",
+          titleWidget: DefaultTextStyle(
+            style: pageDecoration.titleTextStyle,
+            child: AnimatedTextKit(
+              pause: Duration(seconds: 5),
+              repeatForever: false,
+              totalRepeatCount: 4,
+              animatedTexts: [
+                TyperAnimatedText("Manage waste properly"),
+              ],
+            ),
+          ),
+          bodyWidget: Text(
+            "Proper waste management in an environmental friendly manner, preventing uncesssasary wastage",
+            style: pageDecoration.bodyTextStyle,
+            textAlign: TextAlign.center,
+          ),
           image: _buildImage2('undraw_Throw_away_re_x60k.svg'),
           decoration: pageDecoration,
         ),
         PageViewModel(
-          title: "Learn to Conserve Energy",
+          titleWidget: DefaultTextStyle(
+            style: pageDecoration.titleTextStyle,
+            child: AnimatedTextKit(
+              pause: Duration(seconds: 5),
+              repeatForever: false,
+              totalRepeatCount: 4,
+              animatedTexts: [
+                TyperAnimatedText("Learn to Conserve Energy"),
+              ],
+            ),
+          ),
           body:
               "Recieve tips on how to conserve energy in your home. Get ideas on how to save the environment from detrimental activities caused by indutries around us",
           image: _buildImage('windturbin.svg'),
           decoration: pageDecoration,
         ),
         PageViewModel(
-          title: "Promote your Advocacy",
+          titleWidget: DefaultTextStyle(
+            style: pageDecoration.titleTextStyle,
+            child: AnimatedTextKit(
+              pause: Duration(seconds: 5),
+              repeatForever: false,
+              totalRepeatCount: 4,
+              animatedTexts: [
+                TyperAnimatedText("Promote your Advocacy"),
+              ],
+            ),
+          ),
           body:
               "Are you and environnebtal advocate? \nI will help you create awareness of your campeigns",
           image: _buildImage('undraw_Push_notifications_re_t84m (1).svg'),
           decoration: pageDecoration,
         ),
         PageViewModel(
-          title: "Join the team",
+          titleWidget: DefaultTextStyle(
+            style: pageDecoration.titleTextStyle,
+            child: AnimatedTextKit(
+              pause: Duration(seconds: 5),
+              repeatForever: false,
+              totalRepeatCount: 4,
+              animatedTexts: [
+                TyperAnimatedText("Join the team"),
+              ],
+            ),
+          ),
           body:
               "Join a team of passionate and devoted environmentalists and get engaged as an advocates",
           image: _buildImage('undraw_team_spirit_hrr4.svg'),
           decoration: pageDecoration,
         ),
         PageViewModel(
-          title: "Get started",
+          titleWidget: DefaultTextStyle(
+            style: pageDecoration.titleTextStyle,
+            child: AnimatedTextKit(
+              pause: Duration(seconds: 10),
+              repeatForever: true,
+              totalRepeatCount: 4,
+              animatedTexts: [
+                WavyAnimatedText("Get started!"),
+              ],
+            ),
+          ),
           body: "Alright, no more waiting! \nLet's get started now",
           image: _buildImage('img2.jpg'),
           footer: Container(
