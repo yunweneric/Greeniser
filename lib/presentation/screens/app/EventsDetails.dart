3f3c2181d2666e0ae9ffc166a45c043a9076172e
yunweneric <yunweneric@gmail.com>
2021-07-27 18:32:45 +0100

    detail page with container time down

--- lib/presentation/screens/app/EventsDetails.dart
+++ lib/presentation/screens/app/EventsDetails.dart
@@ -1,5 +1,6 @@
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:greeniser/presentation/routes/AppRoutes.dart';
 import 'package:greeniser/presentation/utilities/constants.dart';
 import 'package:greeniser/presentation/widgets/cardDetails.dart';
@@ -32,15 +33,65 @@ class _EventDetailsState extends State<EventDetails> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
-              height: screenHeight(context) / 2,
-              decoration: BoxDecoration(
-                color: kprimaryColor,
-                image: DecorationImage(
-                  image: AssetImage('assets/images/climate4.jpg'),
-                  fit: BoxFit.cover,
+            Stack(
+              children: [
+                Container(
+                  height: screenHeight(context) / 2,
+                  decoration: BoxDecoration(
+                    color: kprimaryColor,
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/climate4.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
-              ),
+                Positioned(
+                  bottom: 0,
+                  child: Container(
+                    width: screenWidth(context),
+                    // height: screenHeight(context) / 2,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10),
+                      ),
+                      color: Colors.black38,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 20.0,
+                        horizontal: 20.0,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            'Time left',
+                            style: GoogleFonts.lato(
+                                textStyle: TextStyle(
+                              fontSize: 15.0,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white,
+                            )),
+                            textAlign: TextAlign.end,
+                          ),
+                          Text(
+                            '12:44:09 min',
+                            style: GoogleFonts.lato(
+                                textStyle: TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white,
+                              fontStyle: FontStyle.italic,
+                            )),
+                            textAlign: TextAlign.end,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
             minorSpacer(context),
             Padding(
